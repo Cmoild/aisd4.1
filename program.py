15d00148c300e08d12d996f2e99c90d1d13f2023
Cmoild <cold17748@gmail.com>
2024-02-14 20:52:39 +0300

    image editing

--- program.py
+++ program.py
@@ -1,7 +1,14 @@
 from PIL import Image
 import numpy as np
 
-image = Image.open("istockphoto-1337005456-612x612.jpg")
-#image.show()
-img = np.asarray(image)
-print(len(img[0]))
+img = np.asarray(Image.open("istockphoto-1337005456-612x612.jpg")).copy()
+
+for i in range(len(img)):
+    for j in range(len(img[0])):
+        if img[i][j] > 100:
+            img[i][j] = 255
+        else:
+            img[i][j] = 0
+
+new_img = Image.fromarray(img)
+new_img.show()
